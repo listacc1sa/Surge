b69401a7b8d26180dd20d204966dbefdaebdeefa
listacc1sa <166372012+listacc1sa@users.noreply.github.com>
2025-02-03 23:14:17 +0300

    Update Rochat.js

---
+++ script/Rochat.js
@@ -0,0 +1,6 @@
+[rewrite_local]
+https://api.rochathub.com/inteco.user.v1.UserRightsService/GetUserRights url script-response-body https://raw.githubusercontent.com/listacc1sa/Ss/refs/heads/main/quantumultx/js/1.js
+https://api.rochathub.com/inteco.user.v1.UserRightsService/GetUserRights url script-response-body https://raw.githubusercontent.com/listacc1sa/Ss/refs/heads/main/quantumultx/js/2.js
+
+[mitm]
+hostname = api.rochathub.com
